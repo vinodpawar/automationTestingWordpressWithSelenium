0160d7a8d286548799a36ed25b7826517b9c73ac
Vinod Pawar <droidvinod@gmail.com>
2023-05-26 09:55:14 +0530

    First commit

---
+++ src/test/java/wordpress/NewTestTest.java
@@ -0,0 +1,25 @@
+package wordpress;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTestTest {
+  @Test
+  public void mahatenders() {
+	  
+	  
+	//*[@id="informal_2"]/td[1]
+	//*[@id="informal_2"]/td[2]
+	//*[@id="informal_3"]/td[1]
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
